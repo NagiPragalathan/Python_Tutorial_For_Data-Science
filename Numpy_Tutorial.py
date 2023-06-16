35c4ccd2af07007e4cce9b5a374d1729358ee211
nagipragalathan <kk2069037@gmail.com>
2023-06-16 20:01:40 +0530

    The CheckPoint Point the JupiterNote Book's Notes

--- Numpy_Tutorial.py
+++ Numpy_Tutorial.py
@@ -1,3 +1,13 @@
 # Array one D array
 
 # we will handlw thw datas as matrix
+
+# NumPy are used to Numarical Computation
+
+# if we use are working using adrino in that time we can use "SciPy" Module with numpy
+
+# Numpy is the multi Dymantioal array
+
+# it is the large collections of mathamatical collections
+
+ 
